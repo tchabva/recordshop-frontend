42fdddd59eac776081def303f33fff756e481aa6
Tawanda Chabva <tchabva@gmail.com>
2024-12-16 11:48:21 +0000

    feat: implement method getAllInStockAlbums in AlbumApiService

--- app/src/main/java/com/northcoders/recordshop/model/service/AlbumApiService.java
+++ app/src/main/java/com/northcoders/recordshop/model/service/AlbumApiService.java
@@ -1,4 +1,14 @@
 package com.northcoders.recordshop.model.service;
 
+import com.northcoders.recordshop.model.Album;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface AlbumApiService {
+
+    @GET("albums")
+    Call<List<Album>> getAllInStockAlbums();
 }
