b5609efdb7b4c1f3d4e9693db9a82d391e29aca9
Tawanda Chabva <tchabva@gmail.com>
2024-12-16 11:11:17 +0000

    feat: create Album model for JSON deserialisation

---
+++ app/src/main/java/com/northcoders/recordshop/model/Album.java
@@ -0,0 +1,15 @@
+package com.northcoders.recordshop.model;
+
+import java.sql.Date;
+
+public class Album {
+    private long id;
+    private String title;
+    private String artist;
+    private String genre;
+    private Date releaseDate;
+    private int stock;
+    private double price;
+    private String dateCreated;
+    private String dateModified;
+}
