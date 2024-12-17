2be6b6fa25ec227a9a8fa8a5051922ab72ea740a
Tawanda Chabva <tchabva@gmail.com>
2024-12-17 10:59:34 +0000

    feat: add getters, setters, allargs & noargs constructors for Album model

--- app/src/main/java/com/northcoders/recordshop/model/Album.java
+++ app/src/main/java/com/northcoders/recordshop/model/Album.java
@@ -12,4 +12,91 @@ public class Album {
     private double price;
     private String dateCreated;
     private String dateModified;
+
+    public Album(long id, String title, String artist, String genre, Date releaseDate, int stock, double price, String dateCreated, String dateModified) {
+        this.id = id;
+        this.title = title;
+        this.artist = artist;
+        this.genre = genre;
+        this.releaseDate = releaseDate;
+        this.stock = stock;
+        this.price = price;
+        this.dateCreated = dateCreated;
+        this.dateModified = dateModified;
+    }
+
+    public Album() {
+    }
+
+    public String getDateModified() {
+        return dateModified;
+    }
+
+    public void setDateModified(String dateModified) {
+        this.dateModified = dateModified;
+    }
+
+    public String getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(String dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
