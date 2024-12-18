ca5e85932e3311c3813c278294e263570eb201c9
Tawanda Chabva <tchabva@gmail.com>
2024-12-18 12:41:28 +0000

    feat(view): implement the clickhandler and bind in the AddNewAlbumActivity

--- app/src/main/java/com/northcoders/recordshop/ui/addalbum/AddNewAlbumActivity.java
+++ app/src/main/java/com/northcoders/recordshop/ui/addalbum/AddNewAlbumActivity.java
@@ -3,14 +3,36 @@ package com.northcoders.recordshop.ui.addalbum;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.northcoders.recordshop.R;
+import com.northcoders.recordshop.databinding.ActivityAddNewAlbumBinding;
+import com.northcoders.recordshop.model.Album;
+import com.northcoders.recordshop.ui.mainactivity.MainActivityViewModel;
 
 public class AddNewAlbumActivity extends AppCompatActivity {
+
+    private ActivityAddNewAlbumBinding binding;
+    private AddAlbumClickHandler handler;
+    private Album album;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_album);
+
+        album = new Album();
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_add_new_album);
+
+        MainActivityViewModel viewModel = new ViewModelProvider(this)
+                .get(MainActivityViewModel.class);
+
+        handler = new AddAlbumClickHandler(album, this, viewModel);
+
+        binding.setAlbum(album);
+
+        binding.setClickHandler(handler);
     }
 }
