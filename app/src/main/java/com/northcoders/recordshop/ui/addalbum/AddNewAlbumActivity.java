fbef907e262c6e9d6758da7ce14c023e1333d000
Tawanda Chabva <tchabva@gmail.com>
2024-12-18 10:59:18 +0000

    feat(view): create AddNewAlbumActivity in addalbum package

---
+++ app/src/main/java/com/northcoders/recordshop/ui/addalbum/AddNewAlbumActivity.java
@@ -0,0 +1,16 @@
+package com.northcoders.recordshop.ui.addalbum;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.northcoders.recordshop.R;
+
+public class AddNewAlbumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_album);
+    }
+}
