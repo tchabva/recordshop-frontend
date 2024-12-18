6ffd8141ccd80805d2029eb8c139d7eec75e1246
Tawanda Chabva <tchabva@gmail.com>
2024-12-18 12:43:07 +0000

    feat(view): create & implement MainActivityClickHandler

---
+++ app/src/main/java/com/northcoders/recordshop/ui/mainactivity/MainActivityClickHandler.java
@@ -0,0 +1,19 @@
+package com.northcoders.recordshop.ui.mainactivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+
+public class MainActivityClickHandler {
+
+    private Context context;
+
+    public MainActivityClickHandler(Context context) {
+        this.context = context;
+    }
+
+    public void onBackButtonClicked(View view){
+        Intent intent = new Intent(context, MainActivity.class);
+        context.startActivity(intent);
+    }
+}
