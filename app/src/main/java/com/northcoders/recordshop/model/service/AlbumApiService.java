a54b29ced9a190e17a1025733ea5cfae7683549f
Tawanda Chabva <tchabva@gmail.com>
2024-12-18 10:44:50 +0000

    feat(service): add addAlbum method to API service

--- app/src/main/java/com/northcoders/recordshop/model/service/AlbumApiService.java
+++ app/src/main/java/com/northcoders/recordshop/model/service/AlbumApiService.java
@@ -5,10 +5,14 @@ import com.northcoders.recordshop.model.Album;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 
 public interface AlbumApiService {
 
     @GET("albums")
     Call<List<Album>> getAllInStockAlbums();
+
+    @GET("albums/add")
+    Call<Album> addAlbum(@Body Album album);
 }
